e7a84f41520f5f7af5bc4b44bd4dfbb035ec4dc9
msp888 <msp888@mail.ru>
2023-12-23 19:16:03 +0300

    Исправляем ошибки

--- task5/task.py
+++ task5/task.py
@@ -41,6 +41,9 @@ def createMatrix(data: list) -> np.array:
     return np.array(raw)
 
 def isStrResultType(str1, str2) -> bool:
+    if (type(str1) == list) or (type(str2) == list):
+        return True
+
     str1 = json.loads(str1)
     str2 = json.loads(str2)
     res = True
@@ -71,38 +74,34 @@ def S(str1, str2, isStr) -> list:
     criterion = matrix12 | matrix12T 
 
     answer = []
+    tmp = []
     for i in range(criterion.shape[0]):
-        for j in range(i):
+        item = []
+        for j in range(criterion.shape[0]): #range(i):
             if criterion[i][j] == 0:
                 if isStr:
-                    answer.append([str(j+1), str(i+1)])
+                    item.append(str(j+1))
                 else:
-                    answer.append([j+1, i+1])
+                    item.append(j+1)
 
-    return answer
+        if len(item) > 0:
+            if isStr:
+                item.append(str(i+1))
+            else:
+                item.append(i+1)
+            tmp.append([item, 0])
 
-def S3(str1, str2, str3, isStr) -> list:
-    str1 = eval(str1)
-    str2 = eval(str2)    
-    str3 = eval(str3)    
-    
-    matrix1 = createMatrix(str1)
-    matrix2 = createMatrix(str2)
-    matrix3 = createMatrix(str3)
-
-    matrix12 = matrix1 * matrix2 * matrix3
-    matrix12T = matrix1.T * matrix2.T * matrix3.T
-
-    criterion = matrix12 | matrix12T 
-
-    answer = []
-    for i in range(criterion.shape[0]):
-        for j in range(i):
-            if criterion[i][j] == 0:
-                if isStr:
-                    answer.append([str(j+1), str(i+1)])
-                else:
-                    answer.append([j+1, i+1])
+    for t in tmp:
+        if t[1] == 0:
+            r = t[0]
+            t[1] = 1
+            for l in tmp:
+                if l[1] == 0:
+                    m = list(set(r) & set(l[0]))
+                    if len(m) > 0:
+                        l[1] = 1
+                        r = list(set(r) | set(l[0]))
+            answer.append(r)
 
     return answer
 
@@ -162,37 +161,37 @@ def f2(str1, str2, isStr) -> list:
 
     resL = []
     for i in range(n):
-        if not isUsed([AL[i][0]], used):
+        isAddA = False
+        if (AL[i][2] == 0) and (AL[i][0] in x) and (not isUsed([AL[i][0]], used)):
+            resL.append(AL[i][0])
+            used.append(AL[i][0])
+            isAddA = True
+
+        isAddB = False
+        if (BL[i][2] == 0) and (BL[i][0] in x) and (not isUsed([BL[i][0]], used)):
+            resL.append(BL[i][0])
+            used.append(BL[i][0])
+            isAddB = True
+
+        if (not isUsed([AL[i][0]], used)) and (not isAddA):
             S_item = getSforItem(AL[i][0], Sab)
             if (len(S_item) > 0):
                     for item in S_item:
                         used.append(item)
                     resL.append(S_item)
 
-        if not isUsed([BL[i][0]], used):
+        if (not isUsed([BL[i][0]], used)) and (not isAddB):
             S_item = getSforItem(BL[i][0], Sab)
             if (len(S_item) > 0):
                     for item in S_item:
                         used.append(item)
                     resL.append(S_item)
 
-        isAddA = False
-        if (AL[i][0] in x) and (not isUsed([AL[i][0]], used)):
-            resL.append(AL[i][0])
-            used.append(AL[i][0])
-            isAddA = True
-
-        isAddB = False
-        if (BL[i][0] in x) and (not isUsed([BL[i][0]], used)):
-            resL.append(BL[i][0])
-            used.append(BL[i][0])
-            isAddB = True
-
         if (not isAddA) and (not isAddB):
             L = list(set(AL[i][3]) & set(BL[i][3]))
             L2 = []
             for item in L:
-                if not (item in x) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
                     L2.append(item)
             if len(L2) == 1:
                 if not isUsed([L2[0]], used):
@@ -206,7 +205,7 @@ def f2(str1, str2, isStr) -> list:
             L = AL[i][3]
             L2 = []
             for item in L:
-                if not (item in x) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
                     L2.append(item)
             if len(L2) == 1:
                 if not isUsed([L2[0]], used):
@@ -220,7 +219,7 @@ def f2(str1, str2, isStr) -> list:
             L = BL[i][3]
             L2 = []
             for item in L:
-                if not (item in x) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
                     L2.append(item)
             if len(L2) == 1:
                 if not isUsed([L2[0]], used):
@@ -233,106 +232,8 @@ def f2(str1, str2, isStr) -> list:
         
     return resL
 
-def f3(str1, str2, str3, isStr) -> list:
-    Sabc = S3(str1, str2, str3, isStr)
-    #print('Sabc:', Sabc)
-
-    fl = []
-    fl.append(flatList(str1))
-    fl.append(flatList(str2))
-    fl.append(flatList(str3))
-    SL2 = flatList2(Sabc)
-    #print('SL2:', SL2)
-
-    used = []
-    n = len(fl[0])
-    x = []
-    for j in range(len(fl)):
-        for i in range(n):
-            if (not (fl[j][i][0] in SL2)) and (not (fl[j][i][0] in x)) and (fl[j][i][2] == 0):
-                x.append(fl[j][i][0])
-    #print('x:', x)
-
-    resL = []
-    for i in range(n):
-        for j in range(len(fl)):
-            if not isUsed([fl[j][i][0]], used):
-                S_item = getSforItem(fl[j][i][0], Sabc)
-                if (len(S_item) > 0):
-                        for item in S_item:
-                            used.append(item)
-                        resL.append(S_item)
-
-        isAdd = np.zeros((len(fl)), bool)
-        for j in range(len(fl)):
-            isAdd[j] = False
-            if (fl[j][i][0] in x) and (not isUsed([fl[j][i][0]], used)):
-                resL.append(fl[j][i][0])
-                used.append(fl[j][i][0])
-                isAdd[j] = True
-
-        if (not isAdd[0]) and (not isAdd[1]) and (not isAdd[2]):
-            L = list(set(fl[0][i][3]) & set(fl[1][i][3]) & set(fl[2][i][3]))
-            L2 = []
-            for item in L:
-                if not (item in x) and (not isUsed([item], used)):
-                    L2.append(item)
-            if len(L2) == 1:
-                if not isUsed([L2[0]], used):
-                    resL.append(L2[0])
-                    used.append(L2[0])
-            elif len(L2) > 1:
-                resL.append(L2)
-                for item in L2:
-                    used.append(item)
-        elif (not isAdd[0]):
-            L = fl[0][i][3]
-            L2 = []
-            for item in L:
-                if not (item in x) and (not isUsed([item], used)):
-                    L2.append(item)
-            if len(L2) == 1:
-                if not isUsed([L2[0]], used):
-                    resL.append(L2[0])
-                    used.append(L2[0])
-            elif len(L2) > 1:
-                resL.append(L2)
-                for item in L2:
-                    used.append(item)
-        elif (not isAdd[1]):
-            L = fl[1][i][3]
-            L2 = []
-            for item in L:
-                if not (item in x) and (not isUsed([item], used)):
-                    L2.append(item)
-            if len(L2) == 1:
-                if not isUsed([L2[0]], used):
-                    resL.append(L2[0])
-                    used.append(L2[0])
-            elif len(L2) > 1:
-                resL.append(L2)
-                for item in L2:
-                    used.append(item)
-        elif (not isAdd[2]):
-            L = fl[2][i][3]
-            L2 = []
-            for item in L:
-                if not (item in x) and (not isUsed([item], used)):
-                    L2.append(item)
-            if len(L2) == 1:
-                if not isUsed([L2[0]], used):
-                    resL.append(L2[0])
-                    used.append(L2[0])
-            elif len(L2) > 1:
-                resL.append(L2)
-                for item in L2:
-                    used.append(item)
-        #print(resL)
-
-    return resL
-
-def task(str1, str2, str3) -> list:
-    return f3(str1, str2, str3, isStrResultType(str1, str2))
+def task(str1, str2) -> list:
+    return f2(str1, str2, isStrResultType(str1, str2))
 
 def main():
     #str1 = '["1", ["2","3"],"4", ["5", "6", "7"], "8", "9", "10"]'
@@ -349,17 +250,29 @@ def main():
 
 
 
+    '''
     str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
     str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
     str3 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
 
-    '''
     str1 = '["1",["2","3"],"4",["5","6","7"],"8","9","10"]'
     str2 = '[["1","2"],["3","4","5"],"6","7","9",["8","10"]]'
     str3 = '["3",["1","4"],"2","6",["5","7","8"],["9","10"]]'
+
+    str1 = '[[1], [2, 3, 4], [5, 6, 7], 8, 9, 10]' 
+    str2 = '[[1, 2, 3], [4, 5], 6, 7, 9, [8, 10]]' 
+    str3 = '[1, 4, 3, 2,6,[5,7,8],[9,10]]'         
+
     '''
 
-    print(task(str1, str2, str3))
+    str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
+    str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
+    str3 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
+
+    print(task(str1, str2))
+    #print(task(str2, str3))
+    #print(task(str1, str3))
+
 
 if __name__ == "__main__":
     main()
